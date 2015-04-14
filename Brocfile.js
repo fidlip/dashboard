8b7219caf2c45c73049a772baef444ccbec30f6e
fidlip <filip.salda@gmail.com>
2015-04-14 14:34:25 +0200

    Updated skeleton

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,19 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+require('coffee-script/register');
 
-var app = new EmberApp();
+var app = new EmberApp({
+    modals: {
+      layout: true,
+      style: true,
+      animation: 'scale'
+    },
+    sassOptions: {
+      ext: "sass"
+    }
+  }
+);
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -16,5 +27,7 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+
+//app.import('bower_components/moment/min/moment-with-locales.js');
 
 module.exports = app.toTree();
